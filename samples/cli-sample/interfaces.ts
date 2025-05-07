22006aabe298170ecab5e33030dfc3959e50a118
jakub.pawlowski <jakub.pawlowski@live.com>
2025-05-07 06:30:53 +0200

    export all required interfaces in one file

--- samples/cli-sample/interfaces.ts
+++ samples/cli-sample/interfaces.ts
@@ -1,7 +1,43 @@
-import { type CommandEmitter, type ProgressEmitter } from "./dependencies/ink-components";
+export type ProgressItemState =
+  | "new"
+  | "pending"
+  | "starting"
+  | "running"
+  | "stopped"
+  | "done"
+  | "error"
+  | "connected"
+  | "disconnected"
+  | "unknown";
+  
+type ProgressUpdate = {
+  state: ProgressItemState;
+  status: string;
+  details?: string;
+  progress?: number;
+};
+
+interface ProgressEmitter {
+  on(event: "log", listener: (path: string, message: string) => void): this;
+  on(
+    event: "update",
+    listener: (path: string, value: Partial<ProgressUpdate>) => void
+  ): this;
+  on(
+    event: "command",
+    listener: (command: string, path: string, args: string[]) => void
+  ): this;
+}
+
+interface CommandEmitter {
+  on(
+    event: "invoke",
+    listener: (command: string, path: string, args: string[]) => void
+  ): this;
+}
 
 export type CliExport = {
-    cli: (params: any) => void;
-    commandEmitter: CommandEmitter;
-    progressEmitter: ProgressEmitter;
-}
+  cli: (params: any) => void;
+  commandEmitter: CommandEmitter;
+  progressEmitter: ProgressEmitter;
+};
